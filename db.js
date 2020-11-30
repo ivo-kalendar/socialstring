6985d8166d9accf9c20a0d8a255b4250cbdbbc3f
Ivo <ivokalendar@icloud.com>
2020-11-30 16:22:58 +0100

    updated libraryes...

--- db.js
+++ db.js
@@ -1,16 +1,24 @@
-
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 // CONFIGURING THE .ENV FOR SECURE VARIABLES  //
-dotenv.config()
-const mongodb = require('mongodb')
-
+dotenv.config();
+const mongodb = require('mongodb');
 
 // MAIN DATABASE CONNECTION //
-mongodb.connect(process.env.CONNECTIONSTRING, {useUnifiedTopology: true}, (err,client)=>{
-	if (err) {throw err}
-	// EXPORTING THE CLIENT AS DB.JS MAKEING AVAILABLE THROU THE WHOLE APP  //
-	module.exports = client
-	// REQUIREING EXPRESS() FOR LISTENING ON PORT AFTER CONNECTED TO THE DATABASE //
-	const app = require('./app')
-	app.listen(process.env.PORT, console.log(`Connected to Database and now Listening on Port: ${process.env.PORT}...`))
-})
+mongodb.connect(
+    process.env.CONNECTIONSTRING,
+    { useUnifiedTopology: true },
+    (err, client) => {
+        if (err) {
+            throw err;
+        }
+        // EXPORTING THE CLIENT AS DB.JS MAKEING AVAILABLE THROU THE WHOLE APP  //
+        module.exports = client;
+        // REQUIREING EXPRESS() FOR LISTENING ON PORT AFTER CONNECTED TO THE DATABASE //
+        const app = require('./app');
+        app.listen(process.env.PORT, () =>
+            console.log(
+                `Connected to Database and now Listening on Port: ${process.env.PORT}...`
+            )
+        );
+    }
+);
